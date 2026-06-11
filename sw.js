cbe1f5f3927d2cb699e52e465c654fd4d575e46e
olivercorby <ocorby@proton.me>
2026-06-10 20:06:25 -0400

    update9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'pricescout-v1';
-const ASSETS = ['/', '/index.html', '/css/style.css', '/js/app.js', '/manifest.json'];
+const ASSETS = ['/', '/app.html', '/css/style.css', '/js/app.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
